a94373ec1292471749e34451cdaf6160098cfe14
Shiv Shankar Kumar <46074348+ShivShankar038@users.noreply.github.com>
2020-06-05 17:53:35 +0530

    build file

---
+++ precache-manifest.33dca1d1ecc0ba8497d2dc62440025bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c2b44f82e720c0f8f9e3c282744c354",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "646c16195049dce83c93",
+    "url": "/react/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "1d05ebd3dbec9ad80b84",
+    "url": "/react/static/css/main.df9e484a.chunk.css"
+  },
+  {
+    "revision": "646c16195049dce83c93",
+    "url": "/react/static/js/2.17dd9341.chunk.js"
+  },
+  {
+    "revision": "ce028d5e2b0507b0fb507336501c1edf",
+    "url": "/react/static/js/2.17dd9341.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1d05ebd3dbec9ad80b84",
+    "url": "/react/static/js/main.9ddd1a88.chunk.js"
+  },
+  {
+    "revision": "371727dd9267b6501984",
+    "url": "/react/static/js/runtime-main.3d95759a.js"
+  }
+]);
